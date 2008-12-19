80314cb135d8cfb65b26ff43f09fcf94beb10ab5
U-BLACKBOOK\Gojomo <gojomo@bitzi.com>
2008-12-19 11:03:35 -0800

    more debug

--- probe.js
+++ probe.js
@@ -1,1 +1,9 @@
+alert('probe1');
+
+$('#scrapedname').ajaxError(function(ev,req,set,err){alert(err)});
+
+alert('probe2');
+
 $.get("http://github.com"), {}, function(data) {$('#target').text(data)});
+
+alert('probe3');
