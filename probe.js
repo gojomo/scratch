e28947ad2299d0a5ec7403567ff7f9426469c84b
U-BLACKBOOK\Gojomo <gojomo@bitzi.com>
2008-12-19 11:08:43 -0800

    rm stray )

--- probe.js
+++ probe.js
@@ -4,6 +4,6 @@ $('#scrapedname').ajaxError(function(ev,req,set,err){alert(err)});
 
 alert('probe2');
 
-$.get("http://github.com"), {}, function(data) {$('#target').text(data)});
+$.get("http://github.com", {}, function(data) {$('#target').text(data)});
 
 alert('probe3');
