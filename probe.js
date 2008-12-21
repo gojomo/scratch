66594cf9675fa1b3ce0efe707866bf50a9d4b22c
U-BLACKBOOK\Gojomo <gojomo@bitzi.com>
2008-12-20 21:54:44 -0800

    cooperating frame contents

--- probe.js
+++ probe.js
@@ -2,5 +2,5 @@ alert('probe:'+document.domain+' '+document.location.href);
 
 document.domain="github.com";
 
-document.write("<iframe id='if' src='http://github.com/gojomo/gojomo.github.com/tree/master'></iframe><scr"+"ipt>alert('second:'+document.domain+' '+document.location.href);function doit(){alert(document.getElementById('if').contentDocument.getElementById('_rrt').innerHTML)}</script><a onclick='doit()'>doit</a>");
+document.write("<iframe id='if' src='http://github.com/gojomo/scratch/raw/master/friendlyframe.html'></iframe><scr"+"ipt>alert('second:'+document.domain+' '+document.location.href);function doit(){alert(document.getElementById('if').contentDocument.getElementById('_rrt').innerHTML)}</script><a onclick='doit()'>doit</a>");
 
