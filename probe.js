2c7b3402dcda3dbc881b86f68189b17ddfa38d7c
U-BLACKBOOK\Gojomo <gojomo@bitzi.com>
2008-12-20 19:12:00 -0800

    set doc.domain

--- probe.js
+++ probe.js
@@ -1,4 +1,6 @@
 alert('probe:'+document.domain+' '+document.location.href);
 
+document.domain="github.com";
+
 document.write("<scr"+"ipt>alert('second:'+document.domain+' '+document.location.href);</script>");
 
