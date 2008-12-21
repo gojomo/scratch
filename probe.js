813d5c08756b7e134d944b141b987c4e46aec33c
U-BLACKBOOK\Gojomo <gojomo@bitzi.com>
2008-12-20 19:25:52 -0800

    /typo

--- probe.js
+++ probe.js
@@ -2,5 +2,5 @@ alert('probe:'+document.domain+' '+document.location.href);
 
 document.domain="github.com";
 
-document.write("<iframe src='http://github.com/gojomo/gojomo.github.com/tree/master'><iframe><scr"+"ipt>alert('second:'+document.domain+' '+document.location.href);function doit(){alert(document.getElementById('if').contentDocument.getElementById('_rrt').innerHTML)}</script><a onclick='doit()'>doit</a>");
+document.write("<iframe src='http://github.com/gojomo/gojomo.github.com/tree/master'></iframe><scr"+"ipt>alert('second:'+document.domain+' '+document.location.href);function doit(){alert(document.getElementById('if').contentDocument.getElementById('_rrt').innerHTML)}</script><a onclick='doit()'>doit</a>");
 
